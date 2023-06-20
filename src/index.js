af4f5c4311037061050ca75e4a1119ce74495490
Tom Bulled <26026015+tombulled@users.noreply.github.com>
2023-06-20 20:46:04 +0100

    :bug: Use a leading slash for /search route (#4)

--- src/index.js
+++ src/index.js
@@ -13,7 +13,7 @@ root.render(
   <BrowserRouter>
     <Routes>
       <Route path="/" element={<App />} />
-      <Route path="search" element={<Search />} />
+      <Route path="/search" element={<Search />} />
     </Routes>
   </BrowserRouter>
 );
